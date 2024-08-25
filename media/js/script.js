f7f200bcd5389324c69c24fc9460ba511039a2e6
igoralmeidasouza <igoralmeida105@gmail.com>
2024-08-24 21:03:50 -0300

    Inclusao de fontes nos outros extratos

--- media/js/script.js
+++ media/js/script.js
@@ -1314,6 +1314,9 @@ function openPaymentHistoryTab(data) {
                 <html>
                     <head>
                         <title>Extrato de Compra</title>
+                        <link rel="preconnect" href="https://fonts.googleapis.com">
+                        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Readex+Pro:wght@160..700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
                         <link rel="stylesheet" href="media/css/estilos.css">
                         <link rel="icon" href="media/img/icones/toothIcone.png">
                     </head>
@@ -1576,6 +1579,9 @@ function openPaymentSalesTab(data) {
                 <html>
                     <head>
                         <title>Extrato de Vendas</title>
+                        <link rel="preconnect" href="https://fonts.googleapis.com">
+                        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Readex+Pro:wght@160..700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
                         <link rel="stylesheet" href="media/css/estilos.css">
                         <link rel="icon" href="media/img/icones/toothIcone.png">
                     </head>
@@ -1733,6 +1739,9 @@ function openExtratoFinalTab(data) {
             '<html> '+
             '<head> '+
                 '<title>Extrato</title> '+
+                '<link rel="preconnect" href="https://fonts.googleapis.com">'+
+                '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>'+
+                '<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Readex+Pro:wght@160..700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">'+
                 '<link rel="stylesheet" href="media/css/estilos.css"> '+
                 '<link rel="icon" href="media/img/icones/toothIcone.png">'+
             '</head> '+
