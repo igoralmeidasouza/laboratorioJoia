f9e8202fba70e6788dcb6e71bf7eb7d6340f7358
igoralmeidasouza <igoralmeida105@gmail.com>
2024-09-14 14:23:12 -0300

    atualização no style

--- index.php
+++ index.php
@@ -164,13 +164,13 @@
                                 <label for="paciente">Nome do Paciente</label>
                             </div>
 
-                            <!-- Botão adicionar items no carrinho -->
+                            <!-- Botão adicionar items no carrinho
                             <div class="botaoCarrinho" onclick="addToCart()" id="botaoCarrinho">
                                 <span>adicionar</span>
                                 <svg width="20" height="20" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg" class="IconHeaderCart" size="20">
                                     <path d="M7.19975 19.2C5.8798 19.2 4.81184 20.28 4.81184 21.6C4.81184 22.92 5.8798 24 7.19975 24C8.51971 24 9.59967 22.92 9.59967 21.6C9.59967 20.28 8.51971 19.2 7.19975 19.2ZM0 0V2.4H2.39992L6.71977 11.508L5.09982 14.448C4.90783 14.784 4.79984 15.18 4.79984 15.6C4.79984 16.92 5.8798 18 7.19975 18H21.5993V15.6H7.70374C7.53574 15.6 7.40375 15.468 7.40375 15.3L7.43974 15.156L8.51971 13.2H17.4594C18.3594 13.2 19.1513 12.708 19.5593 11.964L23.8552 4.176C23.9542 3.99286 24.004 3.78718 23.9997 3.57904C23.9955 3.37089 23.9373 3.16741 23.8309 2.98847C23.7245 2.80952 23.5736 2.66124 23.3927 2.55809C23.2119 2.45495 23.0074 2.40048 22.7992 2.4H5.05183L3.92387 0H0ZM19.1993 19.2C17.8794 19.2 16.8114 20.28 16.8114 21.6C16.8114 22.92 17.8794 24 19.1993 24C20.5193 24 21.5993 22.92 21.5993 21.6C21.5993 20.28 20.5193 19.2 19.1993 19.2Z" fill="white" data-darkreader-inline-fill="" style="--darkreader-inline-fill: #e8e6e3;"></path></g>
                                 </svg>
-                            </div>
+                            </div> -->
                         </div>
 
                         <!-- SVG da dentadura para marcacao do dente -->
@@ -201,11 +201,23 @@
 
                             <style>
                                 .dentadura{position: relative; width: 100%; overflow-x: clip; margin-top: 13px;}
-                                    .dentadura .content{width: 100%; display: flex; border-top: solid 1px lightgrey; padding-top: 25px; gap: 30px}
+                                    .dentadura .content{width: 100%;display: flex;border-top: solid 1px lightgrey;padding-top: 25px;gap: 30px;flex-direction: row-reverse;}
+
+                                    .dentadura .content .inputs{display: flex; flex-direction: column; gap: 10px; justify-content: flex-end;}
+
                                         .dentadura .content .containerInputLabel{max-width: 200px; overflow-x: clip; position: relative; z-index: 1;}
-                                        .dentadura .content .box{position: relative;z-index: 10;width: 100%;max-width: 360px;margin: 0 auto;padding: 5px 10px;}
+                                        .dentadura .content .box{position: relative;z-index: 10;width: 100%;max-width: 420px;margin: 0 auto 0 0;padding: 5px 10px;}
                                             .dentadura .content .box::before{position: absolute;content: '';width: 100%;height: 1px;transform: translate(-50%,-50%);left: 50%;top: 50%;background: #007bff;z-index: -1;pointer-events: none;}
                                             .dentadura .content .box::after{position: absolute; content: ''; width: 1px; height: 100%; transform: translate(-50%,-50%); left: 50%; top: 50%; background: #007bff; z-index: -1; pointer-events: none;}
+
+                                            .dentadura .content .botaoCarrinho{cursor: pointer;max-width: 40px;width: 100%;height: 37px;cursor: pointer;border: solid 1px #6081af;border-radius: 10px;padding: 12px 5px 5px 5px;transition: all 0.3s ease-in;position: relative; margin: 0 10px 0 13px;}
+                                                .dentadura .content .botaoCarrinho:hover{border: solid 1px #198119;}
+                                                .dentadura .content .botaoCarrinho span{position: absolute; font-size: 10.5px; color: #ffffff; width: max-content; background-color: #0071cd; border-radius: 3px; padding: 0px 6px; line-height: 15px; top: -6px; left: -9px; transition: all 0.3s ease-in;}
+                                                    .dentadura .content .botaoCarrinho:hover span{background-color: #198119;}
+                                                .dentadura .content .botaoCarrinho svg{width: 90%;transition: all 0.2s ease-in;}
+                                                    .dentadura .content .botaoCarrinho svg path{transition: all 0.3s ease-in;fill: #6081af;}
+                                                        .dentadura .content .botaoCarrinho:hover svg path{fill: #198119;}
+
 
                                             .dentadura .content .box ul{display: flex;justify-content: space-between;padding: 0 5px 15px;}
                                             .dentadura .content .box ul:last-child{padding: 5px 12px 0 13px;}
@@ -218,14 +230,22 @@
                                                 .dentadura .content .box svg g path:last-child{fill:none;stroke:#000;stroke-miterlimit:10}
                                                 .dentadura .content .box svg g:hover path{fill: #E8F0FE;}
                                                 .dentadura .content .box svg g path.ativo{fill: #0071CD;}
-                                        
+      
                             </style>
 
                             <p>Marcação dentaria</p>
                             <div class="content">
-                                <div class="containerInputLabel">
-                                    <input type="text" id="cor" name="cor" placeholder="Insira a cor">
-                                    <label for="paciente">Cor</label>
+                                <div class="inputs">
+                                    <div class="containerInputLabel">
+                                        <input type="text" id="cor" name="cor" placeholder="Insira a cor">
+                                        <label for="paciente">Cor</label>
+                                    </div>
+                                    <div class="botaoCarrinho" onclick="addToCart()" id="botaoCarrinho">
+                                        <span>adicionar</span>
+                                        <svg width="20" height="20" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg" class="IconHeaderCart" size="20">
+                                            <path d="M7.19975 19.2C5.8798 19.2 4.81184 20.28 4.81184 21.6C4.81184 22.92 5.8798 24 7.19975 24C8.51971 24 9.59967 22.92 9.59967 21.6C9.59967 20.28 8.51971 19.2 7.19975 19.2ZM0 0V2.4H2.39992L6.71977 11.508L5.09982 14.448C4.90783 14.784 4.79984 15.18 4.79984 15.6C4.79984 16.92 5.8798 18 7.19975 18H21.5993V15.6H7.70374C7.53574 15.6 7.40375 15.468 7.40375 15.3L7.43974 15.156L8.51971 13.2H17.4594C18.3594 13.2 19.1513 12.708 19.5593 11.964L23.8552 4.176C23.9542 3.99286 24.004 3.78718 23.9997 3.57904C23.9955 3.37089 23.9373 3.16741 23.8309 2.98847C23.7245 2.80952 23.5736 2.66124 23.3927 2.55809C23.2119 2.45495 23.0074 2.40048 22.7992 2.4H5.05183L3.92387 0H0ZM19.1993 19.2C17.8794 19.2 16.8114 20.28 16.8114 21.6C16.8114 22.92 17.8794 24 19.1993 24C20.5193 24 21.5993 22.92 21.5993 21.6C21.5993 20.28 20.5193 19.2 19.1993 19.2Z" fill="white" data-darkreader-inline-fill="" style="--darkreader-inline-fill: #e8e6e3;"></path></g>
+                                        </svg>
+                                    </div>
                                 </div>
 
                                 <div class="box">
